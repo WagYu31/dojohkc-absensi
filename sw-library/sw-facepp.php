de2e2ccd41b286c4f56ddc785f0bf077b4191b0b
Wahyu Utomo <130083294+WagYu31@users.noreply.github.com>
2026-02-21 18:45:38 +0700

    feat: tambah sw-facepp.php untuk kredensial Face++ Cloud API
    - Kredensial Face++ dipisah ke sw-facepp.php (bisa di-push)
    - sw-config.php include sw-facepp.php via @include_once
    - Face++ API Key & Secret sudah dikonfigurasi

---
+++ sw-library/sw-facepp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * sw-facepp.php
+ * Konfigurasi Face++ Cloud API untuk verifikasi wajah.
+ * https://www.faceplusplus.com/
+ */
+if (!defined('FACEPP_API_KEY')) {
+    define('FACEPP_API_KEY',    'YJI5NpDVKBoMyDZOTTyNay34zJWlDIr_');
+    define('FACEPP_API_SECRET', 'pu8sKXeTDUXxf6r-Q82Wu9EY7-mtB1qN');
+    define('FACEPP_API_URL',    'https://api-us.faceplusplus.com/facepp/v3');
+}
